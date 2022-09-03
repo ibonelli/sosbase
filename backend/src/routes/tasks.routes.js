104b072759b7e921325e7e34d336a8f4a7c65a0d
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2022-09-03 17:14:20 -0300

    Getting CRUD basic structure in place.

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -2,8 +2,24 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World");
+router.get("/tasks", (req, res) => {
+    res.send("Retrieving task list.");
+});
+
+router.get("/tasks/10", (req, res) => {
+    res.send("Retrieving a single task.");
+});
+
+router.post("/tasks", (req, res) => {
+    res.send("Creating tasks.");
+});
+
+router.delete("/tasks", (req, res) => {
+    res.send("Deleting all task.");
+});
+
+router.put("/tasks", (req, res) => {
+    res.send("Updating a task...");
 });
 
 module.exports = router;
