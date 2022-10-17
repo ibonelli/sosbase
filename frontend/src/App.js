b9df61fb4aa794e4b90614291b12e7a8676b69e9
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2022-10-16 22:50:44 -0300

    We add a route to the router so we can show two components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TasksList from "./components/TasksList";
+import TaskForm from "./components/TaskForm";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TasksList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
+        {/* Falta la ruta de editar... */}
       </Routes>
     </BrowserRouter>
   )
